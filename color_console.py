cda8098089c53cf2bdf5038d2909846d2890419e
Andre <andre.burgaud@gmail.com>
2021-03-07 20:10:54 -0600

    Update code to Python 3.9

--- color_console.py
+++ color_console.py
@@ -1,80 +1,38 @@
 """
-
-Colors text in console mode application (win32).
-Uses ctypes and Win32 methods SetConsoleTextAttribute and
-GetConsoleScreenBufferInfo.
-
-$Id: color_console.py 534 2009-05-10 04:00:59Z andre $
-
+Test module color_console. Requires Python 3.
 """
 
-from ctypes import windll, Structure, c_short, c_ushort, byref
+import color_console as cons
+import sys
 
-SHORT = c_short
-WORD = c_ushort
 
-class COORD(Structure):
-  """struct in wincon.h."""
-  _fields_ = [
-    ("X", SHORT),
-    ("Y", SHORT)]
+def test():
+    """Simple Python test for color."""
+    default_colors = cons.get_text_attr()
+    default_bg = default_colors & 0x0070
+    default_fg = default_colors & 0x0007
+    cons.set_text_attr(cons.FOREGROUND_BLUE | default_bg | cons.FOREGROUND_INTENSITY)
+    print("===========================================")
+    cons.set_text_attr(
+        cons.FOREGROUND_BLUE
+        | cons.BACKGROUND_GREY
+        | cons.FOREGROUND_INTENSITY
+        | cons.BACKGROUND_INTENSITY
+    )
+    print("And Now for Something", end=" ")
+    sys.stdout.flush()  # Force writing first part of the line in blue
+    cons.set_text_attr(
+        cons.FOREGROUND_RED
+        | cons.BACKGROUND_GREY
+        | cons.FOREGROUND_INTENSITY
+        | cons.BACKGROUND_INTENSITY
+    )
+    print("Completely Different!")
+    cons.set_text_attr(default_colors)
+    cons.set_text_attr(cons.FOREGROUND_RED | default_bg | cons.FOREGROUND_INTENSITY)
+    print("===========================================")
+    cons.set_text_attr(default_colors)
 
-class SMALL_RECT(Structure):
-  """struct in wincon.h."""
-  _fields_ = [
-    ("Left", SHORT),
-    ("Top", SHORT),
-    ("Right", SHORT),
-    ("Bottom", SHORT)]
 
-class CONSOLE_SCREEN_BUFFER_INFO(Structure):
-  """struct in wincon.h."""
-  _fields_ = [
-    ("dwSize", COORD),
-    ("dwCursorPosition", COORD),
-    ("wAttributes", WORD),
-    ("srWindow", SMALL_RECT),
-    ("dwMaximumWindowSize", COORD)]
-
-# winbase.h
-STD_INPUT_HANDLE = -10
-STD_OUTPUT_HANDLE = -11
-STD_ERROR_HANDLE = -12
-
-# wincon.h
-FOREGROUND_BLACK     = 0x0000
-FOREGROUND_BLUE      = 0x0001
-FOREGROUND_GREEN     = 0x0002
-FOREGROUND_CYAN      = 0x0003
-FOREGROUND_RED       = 0x0004
-FOREGROUND_MAGENTA   = 0x0005
-FOREGROUND_YELLOW    = 0x0006
-FOREGROUND_GREY      = 0x0007
-FOREGROUND_INTENSITY = 0x0008 # foreground color is intensified.
-
-BACKGROUND_BLACK     = 0x0000
-BACKGROUND_BLUE      = 0x0010
-BACKGROUND_GREEN     = 0x0020
-BACKGROUND_CYAN      = 0x0030
-BACKGROUND_RED       = 0x0040
-BACKGROUND_MAGENTA   = 0x0050
-BACKGROUND_YELLOW    = 0x0060
-BACKGROUND_GREY      = 0x0070
-BACKGROUND_INTENSITY = 0x0080 # background color is intensified.
-
-stdout_handle = windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
-SetConsoleTextAttribute = windll.kernel32.SetConsoleTextAttribute
-GetConsoleScreenBufferInfo = windll.kernel32.GetConsoleScreenBufferInfo
-
-def get_text_attr():
-  """Returns the character attributes (colors) of the console screen
-  buffer."""
-  csbi = CONSOLE_SCREEN_BUFFER_INFO()
-  GetConsoleScreenBufferInfo(stdout_handle, byref(csbi))
-  return csbi.wAttributes
-
-def set_text_attr(color):
-  """Sets the character attributes (colors) of the console screen
-  buffer. Color is a combination of foreground and background color,
-  foreground and background intensity."""
-  SetConsoleTextAttribute(stdout_handle, color)
+if __name__ == "__main__":
+    test()
